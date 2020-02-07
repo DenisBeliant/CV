7bc964fd069a237052aa1b3b50d006336d0d0a36
DenisBeliant <denis.beliant@le-campus-numerique.fr>
2020-02-07 16:36:04 +0100

    Envoie de mon dossier CV sur le repot

---
+++ CV/js/script.js
@@ -0,0 +1,16 @@
+console.log("test");
+$(function() {
+
+    $("#photo").click(function() {
+
+            $("#imageEnGrand").fadeIn('slow');
+
+    });
+
+    $("#imageEnGrand").click(function() {
+
+            $('#imageEnGrand').fadeOut("slow");
+        
+});
+ 
+});
